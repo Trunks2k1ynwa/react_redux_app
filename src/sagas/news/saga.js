53e3b4fdeb95dbae1b651db9884eaef4f596b069
Trunks <Trungthekop123@gmail.com>
2023-02-28 22:57:08 +0700

    push

--- src/sagas/news/saga.js
+++ src/sagas/news/saga.js
@@ -2,8 +2,9 @@ import { takeLatest } from "redux-saga/effects";
 import handleGetNews from "./handlers";
 import { getNews } from "./newSlice";
 
+// Generator function
 export function* newsSaga() {
-  // gọi api 10 lần thì chỉ lấy cái cuối cùng
+  // gọi api 10 lần thì chỉ lấy cái cuối cùng, lấy giá trị distpatch cuối cùng
     yield takeLatest(getNews.type, handleGetNews);
   // takeEvery : call sau mỗi lần action
   }
